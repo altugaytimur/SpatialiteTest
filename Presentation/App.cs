082620f9d2419a255bd1dbd8ad1e064f1e11de9f
Altug Aytimur <aytimuraltug@gmail.com>
2024-05-03 01:14:38 +0300

    Business, Data Access and Presentation layers created

---
+++ Presentation/App.cs
@@ -0,0 +1,49 @@
+﻿using Business.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation
+{
+    public class App
+    {
+        private readonly IBuiildingService _buildingService;
+        private readonly IDoorService _doorService;
+
+        public App(IBuiildingService buildingService, IDoorService doorService)
+        {
+            _buildingService = buildingService;
+            _doorService = doorService;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Binaları Oluştur\n2. Kapı Kontrolü\n3. Kapı Sonuçlarını Yazdır\n4. Çıkış Yap\nLütfen bir seçenek giriniz:");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        _buildingService.CreateBuildingsFromNodes();
+                        break;
+                    case "2":
+                        _doorService.CheckDoorsInsideBuildings();
+                        break;
+                    case "3":
+                        _doorService.PrintDoorResults();
+                        break;
+                    case "4":
+                        Console.WriteLine("Programdan çıkılıyor...");
+                        return;
+                    default:
+                        Console.WriteLine("Geçersiz seçenek. Lütfen tekrar deneyin.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ SpatialiteTest/Extentions/ServicesExtentions.cs
@@ -0,0 +1,53 @@
+﻿using Business.Contracts;
+using Business.Services;
+using DAL;
+using Microsoft.Extensions.DependencyInjection;
+using Presentation;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpatialiteTest.Extentions
+{
+    public static class ServicesExtentions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureServices(this IServiceCollection services )
+        {
+            var dbService = new DbService(GetConnectionString());
+
+            services.AddSingleton(dbService);
+            services
+                .AddScoped<SQLiteConnection>(provider =>
+                        provider.GetService<DbService>().CreateConnection());
+
+            services.AddScoped<IBuiildingService,BuildingService>();
+            services.AddScoped<IDoorService,DoorService>();
+            services.AddSingleton<App>();
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private static string GetConnectionString()
+        {
+            string pathTemplate = @"C:\Users\HP\Desktop\SpatialiteTest\SpatialiteTest\SpatialiteTest\template.sqlite";
+            string destDbFilename = @"C:\Users\HP\Desktop\SpatialiteTest\SpatialiteTest\SpatialiteTest\buildingDatabaseNew.sqlite";
+            if (File.Exists(destDbFilename))
+            {
+                File.Delete(destDbFilename);
+            }
+            File.Copy(pathTemplate, destDbFilename, true);
+
+            string connectionString = $"Data Source={destDbFilename};Version=3;";
+            return connectionString;
+        }
+    }
+}
